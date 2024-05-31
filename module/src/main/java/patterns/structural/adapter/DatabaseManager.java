d80e7df243347b78cbce883afe0de0c5e8d2e2e2
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-05-31 20:05:42 +0200

    Implemented ADAPTER PATTERN.

---
+++ module/src/main/java/patterns/structural/adapter/DatabaseManager.java
@@ -0,0 +1,6 @@
+package patterns.structural.adapter;
+
+//target interface
+public interface DatabaseManager {
+    void connectToDatabase(String driver, String URL, String username, String password);
+}
---
+++ module/src/main/java/patterns/structural/adapter/UserAdaptee.java
@@ -0,0 +1,22 @@
+package patterns.structural.adapter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UserAdaptee extends NetworkUser {
+
+    public Connection connectUserToDatabase(NetworkUser user) {
+        Connection connection = null;
+        try {
+            Class.forName(user.getDriver());
+            connection = DriverManager.getConnection(user.getURL(), user.getUsername(), user.getPassword());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+}
