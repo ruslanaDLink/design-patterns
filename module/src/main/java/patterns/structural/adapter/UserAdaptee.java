dc60258c871e11ed0a65ea01b08e437936a2da87
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-05-31 20:22:42 +0200

    Applied changes.

--- module/src/main/java/patterns/structural/adapter/UserAdaptee.java
+++ module/src/main/java/patterns/structural/adapter/UserAdaptee.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class UserAdaptee extends NetworkUser {
+public class UserAdaptee{
 
     public Connection connectUserToDatabase(NetworkUser user) {
         Connection connection = null;
