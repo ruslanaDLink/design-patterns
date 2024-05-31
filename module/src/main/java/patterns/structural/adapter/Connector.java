35350733a90e0fbcfe079ae045bdc3aa3faab2b0
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-05-31 20:35:07 +0200

    Applied changes.

--- module/src/main/java/patterns/structural/adapter/Connector.java
+++ module/src/main/java/patterns/structural/adapter/Connector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class UserAdaptee{
+public class Connector {
 
     public Connection connectUserToDatabase(NetworkUser user) {
         Connection connection = null;
--- module/src/main/java/patterns/structural/adapter/DatabaseManager.java
+++ module/src/main/java/patterns/structural/adapter/DatabaseManager.java
@@ -2,5 +2,7 @@ package patterns.structural.adapter;
 
 //target interface
 public interface DatabaseManager {
+
     void connectToDatabase(String driver, String URL, String username, String password);
+
 }
